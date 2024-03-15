aae0153108bfeca529590a346cb286d19750c4a0
oxalcvasquez <oxalc0101@gmail.com>
2024-03-15 12:43:17 -0500

    Adding CORS configuration

--- src/main/java/com/oxalc/contactapi/constant/Constant.java
+++ src/main/java/com/oxalc/contactapi/constant/Constant.java
@@ -2,5 +2,6 @@ package com.oxalc.contactapi.constant;
 
 public class Constant {
 
+    public static final String X_REQUESTED_WITH = "X-Requested-With";
     public static final String PHOTO_DIRECTORY = System.getProperty("user.home") + "/Descargas/Uploads/";
 }
